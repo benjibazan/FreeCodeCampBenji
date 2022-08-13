ac58ca966832047553df4b0380370a48d0529880
Benji Bazan <benjirbc@gmail.com>
2022-08-13 12:39:36 -0500

    h1 added

---
+++ javascript/javascript.js
@@ -0,0 +1,16 @@
+// Queue - Stand in line
+function nextInLine(arr, item) {
+    array.push(item);
+    return array.shift();
+ 
+}
+const array = ["benji", "bazan"];
+console.log("Before: " + JSON.stringify(array));
+console.log(nextInLine(array, "ricardo"));
+console.log("After: " + JSON.stringify(array));
+ 
+// practica 2
+
+const numbers = [1,2,3,4,5];
+
+functio
